ba4bac2d86149d55006c2cba7e149d16b3ff7f22
Paul-surf <fanprodkaqw@hotmail.com>
2022-03-24 10:58:11 +0100

    kata 8 solution

---
+++ Kata 8/BasicMathematicalOperations.js
@@ -0,0 +1,7 @@
+function basicOp(operation, value1, value2)
+{
+  var rules = {"+": value1 + value2, "-": value1 - value2, "*": value1 * value2, "/": value1 / value2};
+  return rules[operation]
+}
+
+// Depending on the task, it will give the proper operator
